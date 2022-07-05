f9b715295c20a9f1a96dd8b9d33cc9b11604e7ad
Matthew Mong <matthew.mong1999@gmail.com>
2022-07-04 17:24:44 -0700

    added svelte list

--- src/ListItem.svelte
+++ src/ListItem.svelte
@@ -1,20 +1,34 @@
 <script>
-	export let value;
-	export let size;
+  export let value;
+  export let size;
+  const text = value.join(" ");
+  const sample_sizes = size.map((pos) => ({
+    text: value.slice(pos.start, pos.end).join(" "),
+    start: pos.start,
+    end: pos.end,
+  }));
 </script>
 
-<style>
-	.card {
-		margin: 0.5em;
-		padding: 0.5em 0.5em 0.5em 0.5em;
-		border: 1px solid #eee;
-		border-radius: 4px;
-		box-shadow: 2px 2px 4px rgba(0,0,0,0.1);
-		min-height: 5em;
-	}
-</style>
-
-<div class='card'>
-	<p>{value.join(' ')}</p>
-	<p>{size.map((pos)=> (value.slice(pos.start, pos.end).join(' ')))}</p>
+<div class="card">
+  <p>{text}</p>
+  <select name="samplesize" id="samplesizelist">
+    {#each sample_sizes as todo, index (todo.start)}
+      <option id={index}>
+        {todo.text}
+	  </option>
+    {:else}
+      Nothing
+    {/each}
+	</select>
 </div>
+
+<style>
+  .card {
+    margin: 0.5em;
+    padding: 0.5em 0.5em 0.5em 0.5em;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+    min-height: 5em;
+  }
+</style>
--- src/backend/age_matcher.py
+++ src/backend/age_matcher.py
@@ -4,6 +4,7 @@ def get_age(nlp, text):
     age_matcher = Matcher(nlp.vocab)
     age_category = [{"LEMMA":{"IN": ["adult", "teen", "child", "infant"]}}]
     age_year = [{"LEMMA":{"IN": ["age, year"]}}]
+    age_year = [{"ENT_TYPE": {"IN" : ["CARDINAL"]}, "OP": "+"}, {"LEMMA":{"IN": ["age, year"]}}]
     age_matcher.add('category', [age_category])
     age_matcher.add('year', [age_year])
     ages = age_matcher(text)
