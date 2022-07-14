7de7becdd8b8e9649a68d42467eed13c716a2d53
Matthew Mong <matthew.mong1999@gmail.com>
2022-07-14 16:32:37 -0700

    dont open new window

--- src/CSV.svelte
+++ src/CSV.svelte
@@ -9,19 +9,33 @@
     let rows = items.map((item) => {
       return Object.values(item.selected).map((selected) => {
         if (selected.length > 0) {
-          return '"' + selected.map((pos) =>
-            item.response.input.slice(pos.start, pos.end).join(" ")
-          ).join("\n") + '"';
+          return (
+            '"' +
+            selected
+              .map((pos) =>
+                item.response.input.slice(pos.start, pos.end).join(" ")
+              )
+              .join("\n") +
+            '"'
+          );
         } else {
           return [];
         }
       });
     });
-    console.log(rows);
     let csvContent =
-      "data:text/csv;charset=utf-8," + headers.concat(rows).map((e) => e.join(",")).join("\n");
+      "data:text/csv;charset=utf-8," +
+      headers
+        .concat(rows)
+        .map((e) => e.join(","))
+        .join("\n");
     var encodedUri = encodeURI(csvContent);
-    window.open(encodedUri);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "my_data.csv");
+    document.body.appendChild(link); // Required for FF
+
+    link.click();
   }}
 >
   Export
